2f13236b2bac6a1de221157f5cf821b8d5931031
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-02-28 19:49:46 +0530

    added routes

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { GetAllTasks,AddTask,DeleteTask,GetSingleTask,UpdateTask } from "../controller/task.controller";
+import auth from "../middleware/auth.js";
 const taskSchema = {
     body : {
         type:"object",
@@ -11,11 +12,12 @@ const taskSchema = {
     }
 }
  async function taskRoute(fastify:FastifyInstance,options:any){
-    fastify.get("/all",GetAllTasks) // all task
-    fastify.post("/add",{schema:taskSchema},AddTask) // add task
-    fastify.delete("/:id",DeleteTask) // delete task
-    fastify.put("/:id",UpdateTask) // update task
-    fastify.get("/:id",GetSingleTask) // Single task
+    fastify.get("/all",{preHandler:auth},GetAllTasks) // all task
+    fastify.post("/add",{schema:taskSchema,preHandler:auth},AddTask) // add task
+    fastify.delete("/:id",{preHandler:auth},DeleteTask) // delete task
+    fastify.put("/:id",{preHandler:auth},UpdateTask) // update task
+    fastify.get("/:id",{preHandler:auth},GetSingleTask) // Single task
+    
     
 }
 export default taskRoute
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,29 @@
 import { FastifyInstance } from "fastify";
 import { UserLogin,UserRegister } from "../controller/user.controller";
-
+const userRegisterSchema = {
+    body:{
+        type:"object",
+        required:["email","name","password"],
+        properties:{
+            email:{type:"string"},
+            name:{type:"string"},
+            password:{type:"string"},
+        }
+    }
+}
+const userLoginSchema = {
+    body:{
+        type:"object",
+        required:["email","password"],
+        properties:{
+            email:{type:"string"},
+            password:{type:"string"},
+        }
+    }
+}
 async function userRoute(fastify:FastifyInstance,options:any){
-    fastify.post('/login',UserLogin)
-    fastify.post('/register',UserRegister)
+    fastify.post('/login',{schema:userLoginSchema},UserLogin)
+    fastify.post('/register',{schema:userRegisterSchema},UserRegister)
     
 }
 
