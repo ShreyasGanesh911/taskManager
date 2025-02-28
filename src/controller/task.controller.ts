95410958af7b145d812183165bfc74404a9ee96a
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-02-28 19:49:21 +0530

    added controllers

--- src/controller/task.controller.ts
+++ src/controller/task.controller.ts
@@ -1,15 +1,30 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import AsyncHandler from "../utils/asynchandler.js";
 import Task from "../models/task.js";
-interface CustomRequest extends FastifyRequest{
+interface CustomRequest<T = any> extends FastifyRequest{
     user:{
         name:string,email:string,_id:string
-    }
+    };
+    body:T
+
+}
+const set = ["low","medium","high"]
+interface CustomRequestWithQuery<T = any> extends FastifyRequest{
+    user:{
+        name:string,email:string,_id:string
+    };
+    query:T
+
 }
 const success = true
-export const GetAllTasks = AsyncHandler(async(req:CustomRequest,reply:FastifyReply)=>{
+export const GetAllTasks = AsyncHandler(async(req:CustomRequestWithQuery<{priority:string}>,reply:FastifyReply)=>{
+    const {priority} = req.query
     const id = req.user._id
-    const tasks = await Task.find({user:id})
+    let tasks
+    if(priority && set.includes(priority))
+        tasks = await Task.find({$and:[{user:id },{priority}]})
+    else  tasks = await Task.find({user:id})
+     
     reply.status(200).send({
         success,
         tasks,
@@ -19,24 +34,40 @@ export const GetAllTasks = AsyncHandler(async(req:CustomRequest,reply:FastifyRep
 export const GetSingleTask= AsyncHandler(async(req:FastifyRequest<{ Params: { id: string } }>,reply:FastifyReply)=>{
     const {id} = req.params
     const task = await Task.findById({_id:id})
+    if(!task)
+        return reply.status(404).send({message:"Task not found"})
     reply.status(200).send({
         success,
         task,
         message:"Task"
     })
 })
-export const DeleteTask = AsyncHandler(async(req:FastifyRequest,reply:FastifyReply)=>{
+export const DeleteTask = AsyncHandler(async(req:FastifyRequest<{Params:{id:string}}>,reply:FastifyReply)=>{
+    const {id} = req.params
+    const task = await Task.findByIdAndDelete({_id:id})
+    if(!task)
+        return reply.status(404).send({message:"Task not found"})
     reply.status(200).send({
-        message:"Test"
+        message:`Task: ${task.title} is deleted `
     })
 })
-export const UpdateTask = AsyncHandler(async(req:FastifyRequest,reply:FastifyReply)=>{
+export const UpdateTask = AsyncHandler(async(req:FastifyRequest<{Params:{id:string},Body:{title:string,description:string,priority:string}}>,reply:FastifyReply)=>{
+    const {id} = req.params
+    const {title,description,priority} = req.body
+    const task = await Task.findByIdAndUpdate({_id:id},{title,description,priority},{new:true})
+    if(!task)
+        return reply.status(404).send({message:"Task not found"})
     reply.status(200).send({
-        message:"Test"
+        message:`Task updated`,
+        title:task.title
+        
     })
 })
-export const AddTask = AsyncHandler(async(req:FastifyRequest,reply:FastifyReply)=>{
+export const AddTask = AsyncHandler(async(req:CustomRequest<{title:string,description:string,priority:string}>,reply:FastifyReply)=>{
+    const {description,priority,title} = req.body
+    await Task.create({description,priority,title,user:req.user._id})
     reply.status(200).send({
-        message:"Test"
+        
+        message:"Task created"
     })
-})
+}) 
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,13 +1,30 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import AsyncHandler from "../utils/asynchandler.js";
+import User from "../models/User.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+export const UserLogin = AsyncHandler(async(req:FastifyRequest<{Body:{email:string,password:string}}>,reply:FastifyReply)=>{
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user) return reply.code(401).send({message:"Invalid email or password"})
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch)
+        return reply.code(401).send({message:"Invalid email or password"})
+    const cred = await User.findOne({email}).select("-password")
+    if(cred){
+    const token = await jwt.sign(cred.toObject(),"SOME KEY",{expiresIn:"1d"})
+    reply.setCookie("auth",token,{path:'/'}).status(200).send({ 
+        message:"Logged in successfully",
+        cred
+    })
+    }
+})
+export const UserRegister = AsyncHandler(async(req:FastifyRequest<{Body:{email:string,password:string,name:string}}>,reply:FastifyReply)=>{
+    const {email,password,name} = req.body;
+    const hashedPassword = await bcrypt.hash(password,10);
+    await User.create({email,password:hashedPassword,name});
 
-export const UserLogin = AsyncHandler(async(req:FastifyRequest,reply:FastifyReply)=>[
     reply.status(200).send({
-        message:"Working fine"
+        message:"User created!"
     })
-])
-export const UserRegister = AsyncHandler(async(req:FastifyRequest,reply:FastifyReply)=>[
-    reply.status(200).send({
-        message:"Working fine"
-    })
-])
+})
