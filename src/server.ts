bcb8455cff130d26eaa2967b26439bfd23d8420d
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-02-28 19:48:53 +0530

    added fastify-cookie

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import Fastify from "fastify";
 import "dotenv/config"
+import cookie from "fastify-cookie";
 import connection from "./db/db.js";
 import taskRoute from "./routes/task.route.js";
 import userRoute from "./routes/user.route.js";
-const port = process.env.PORT || 8000
+const port = process.env.PORT ? parseInt(process.env.PORT,10) : 8000
 const fastify = Fastify({
     logger:true
 })
+fastify.register(cookie)
 fastify.register(taskRoute,{prefix:"/api/v1/task"})
 fastify.register(userRoute,{prefix:"/api/v1/user"})
 
-fastify.listen({port:8000},(err)=>{
+fastify.listen({port},(err)=>{
     connection().then(()=>console.log("Connected to db")).catch(e=>console.log("Error connecting to db",e))
     if(err){
         console.log(err)
